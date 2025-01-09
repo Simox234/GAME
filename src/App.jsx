ca5f4474df06570140ffc362395541dc76a0566f
mouhssineidbella <mouhssineidbella@gmail.com>
2025-01-09 11:25:32 +0100

    i idded signin and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
-import Login from './login'; 
-import Hero from './Hero.jsx'
-import Shop from './Shop.jsx'
-
+import Login from './login';
+import Signup from './signUp';
+import Slider from './Slider';
+import Bits from './Bits';
+import Hero from './Hero';
+import Shop from './Shop';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      
-      <Routes>
+      <div className="flex flex-col min-h-screen">
+        {/* Navbar - Always Visible */}
+        <Navbar />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Hero />} />
-        <Route path="/shop" element={<Shop />} />
-        
-      </Routes>
+        {/* Main Content Area */}
+        <main className="flex-grow">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Slider />
+                  <Hero />
+                  <Bits />
+                  <Shop />
+                </>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </main>
+
+        {/* Footer - Always Visible */}
+        <Footer />
+      </div>
     </Router>
   );
 };
